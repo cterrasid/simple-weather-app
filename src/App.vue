f5d9f864c81413ae966a64361e0e344da279334b
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-08-31 12:57:08 +0200

    Created styles for the whater box

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,16 @@
       <div :class="$style.searchBox">
         <input type="text" :class="$style.searchBar" placeholder="Search..." />
       </div>
+      <div :class="$style.weatherWrap">
+        <div :class="$style.locationBox">
+          <div :class="$style.location">Madrid, Spain</div>
+          <div :class="$style.date">Monday 31 August 2020</div>
+        </div>
+        <div :class="$style.weatherBox">
+          <div :class="$style.temperature">9ºC</div>
+          <div :class="$style.weather">Rain</div>
+        </div>
+      </div>
     </main>
   </div>
 </template>
@@ -60,7 +70,48 @@ body {
 
 .search-box .search-bar:focus {
   background-color: #ffffffbf;
-  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 0px 16px #00000040;
   border-radius: 0px 16px 0px 16px;
+}
+
+.locationBox .location {
+  color: #ffffff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px #00000040;
+}
+
+.locationBox .date {
+  color: #ffffff;
+  font-size: 20px;
+  font-weight: 300;
+  text-align: center;
+  font-style: italic;
+}
+
+.weatherBox {
+  text-align: center;
+}
+
+.weatherBox .temperature {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #ffffff;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 1px 3px #00000040;
+  background-color: #ffffff40;
+  border-radius: 16px;
+  margin: 30px 0px;
+  box-shadow: 3px 6px #00000040;
+}
+
+.weatherBox .weather {
+  color: #ffffff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px #00000040;
 }
 </style>
